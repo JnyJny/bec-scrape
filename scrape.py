3355eba97ece473f70481422fdca985a32b0799b
Erik O'Shaughnessy <erik.oshaughnessy@gmail.com>
2017-03-11 14:02:07 -0600

    added project image de-duplication spanning all project

--- scrape.py
+++ scrape.py
@@ -4,8 +4,44 @@ from project import Project
 from urls import urls
 import json
 
+
+def build_image_catalog(projects):
+    '''
+    '''
+    catalog = {}
+    for p in projects:
+        for image in p.images:
+            n = catalog.setdefault(image, 0)
+            catalog[image] = n + 1
+
+    return catalog
+
+def deduplicate_project_images(projects, others=None):
+    '''
+
+    '''
+    
+    catalog = build_image_catalog(projects)
+
+    duplicates = set([k for k,v in catalog.items() if v > 1])
+
+    if others:
+        duplicates.update(set(others))
+
+    for p in projects:
+        p.images = list(set(p.images).difference(duplicates))
+
 if __name__ == '__main__':
-    data = [Project(url).orderedDict for url in urls]
-    with open('bec.json','wb') as output:
-        output.write(bytes(json.dumps(data, indent=2),'utf-8'))
+    '''
+    '''
+
+    junk = ['../../images/head_latestnews.gif']
+    
+    projects = [Project(url) for url in urls]
+    
+    deduplicate_project_images(projects, junk)
+    
+    projectRecords = [p.record for p in projects]
+    
+    print(json.dumps(projectRecords, indent=2))
                      
